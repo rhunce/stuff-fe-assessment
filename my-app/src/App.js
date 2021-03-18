a3da2a65573640891706afcaa803b43de77b8d91
Raymond Hunce <hunce18c@gmail.com>
2021-03-18 19:49:51 -0400

    Complete left section of page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,15 +5,12 @@ import DateOfBirthInput from './DateOfBirthInput.js';
 function App() {
   return (
     <div className="container">
-      <div>
-        <div>03.08.Date of</div>
-        <div className="check"></div><span>ADS-36</span>
+      <div className="section-1">
+        <div className="date-of">03.08.Date of</div>
+        <div className="ads-36"><div className="check"></div><span>ADS-36</span></div>
       </div>
       <div className="App">
         <DateOfBirthInput />
-      </div>
-      <div>
-        Section 2
       </div>
     </div>
   );
